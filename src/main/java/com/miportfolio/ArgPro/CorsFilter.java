83770dce2ab3b0a12c5e9d4e360a41b98f540c61
IvonOcano <ivonocano@outlook.com>
2023-03-25 13:03:12 -0300

    seguimos intentando

---
+++ src/main/java/com/miportfolio/ArgPro/CorsFilter.java
@@ -0,0 +1,32 @@
+package com.miportfolio.ArgPro;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // No se necesita inicialización
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "https://portfolio-backend-ghx4.onrender.com"); 
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // No se necesita destrucción
+    }
+}
