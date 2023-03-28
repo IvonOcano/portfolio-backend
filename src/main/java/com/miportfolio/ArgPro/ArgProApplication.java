0f4ec54159a268790cfe06f481e792b546d38953
IvonOcano <ivonocano@outlook.com>
2023-03-28 16:50:34 -0300

    falta menos

--- src/main/java/com/miportfolio/ArgPro/ArgProApplication.java
+++ src/main/java/com/miportfolio/ArgPro/ArgProApplication.java
@@ -2,12 +2,24 @@ package com.miportfolio.ArgPro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ArgProApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArgProApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://nuevoivonportf.web.app").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
