2df5cb218025fe2eb80a214da7c732c92c4929ae
IvonOcano <ivonocano@outlook.com>
2023-03-29 15:15:22 -0300

    config

--- src/main/java/com/miportfolio/ArgPro/WebConfig.java
+++ src/main/java/com/miportfolio/ArgPro/WebConfig.java
@@ -1,32 +1,14 @@
 package com.miportfolio.ArgPro;
 
-import java.util.Arrays;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-      @Override
+
+public class WebConfig implements WebMvcConfigurer{
+    @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-              .allowedOrigins("https://nuevoivonportf.web.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");  
-                
-    }
-     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://nuevoivonportf.web.app/"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        registry.addMapping("/**");
     }
 }
-
